f51ea9b07b4608ae5f68fb9e3d986ed1a4122a65
James-Fraser1 <jdfraser2022@gmail.com>
2022-10-25 22:37:03 -0400

    rules added for config.js page for loading styles

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -30,12 +30,36 @@ module.exports = () => {
         theme_color: '#7058bf',
         start_url: '/',
         publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          }
+        ]
       })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
