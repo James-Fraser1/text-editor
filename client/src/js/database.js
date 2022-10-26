5b65445900fdfb24f5b098449da60a48223218cb
James-Fraser1 <jdfraser2022@gmail.com>
2022-10-25 20:56:26 -0400

    logic added to database.js for database connectivity

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,17 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+
+  console.error('putDb not implemented');
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
 
+
+  console.error('getDb not implemented');
+}
 initdb();
