35f2abc8dd776ecf7d2fc105539b9ea1a9dd8ac1
James-Fraser1 <jdfraser2022@gmail.com>
2022-10-25 21:10:16 -0400

    transaction const creation for database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,15 +14,26 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
+  // Const used to connect to the database
   const jateDb = await openDB('jate', 1);
+  // Const used to create a new transaction with "jate" and "readwrite" requirements
+  const tx = jateDb.transaction('jate', 'readwrite');
+  // 
+
+  // 
 
   console.error('putDb not implemented');
 }
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
+  // Const used to connect to the database
   const jateDb = await openDB('jate', 1);
+  // Const used to create a new transaction with "jate" requirement
+  const tx = jateDb.transaction('jate');
+  // 
 
+  // 
 
   console.error('getDb not implemented');
 }
